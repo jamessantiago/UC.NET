2e32c89d4e33fed7d6939999f6ad2d9ecf478a7f
jamessantiago <james.r.santiago@gmail.com>
2015-11-09 13:27:58 +0900

    Added AxlExtensions

---
+++ UC.NET/UcClient/AxlClient/AxlExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxlNetClient
+{
+    public static class AxlExtensions
+    {
+        public static XFkType DefaultOrNullIfEmpty(this XFkType data)
+        {
+            return string.IsNullOrEmpty(data.uuid) && string.IsNullOrEmpty(data.Value) ? null : data;
+        }
+    }
+}
